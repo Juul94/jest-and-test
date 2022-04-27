d39bd23a71709ba24d8cbb5ea65adb1945e346ee
Alexander Sørensen <juul.94@hotmail.com>
2022-04-28 00:35:00 +0200

    Testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = React.useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search value={search} onChange={handleChange}>
+        Search
+      </Search>
+
+      <p>
+        Searches for: <span className="result">{search ? search : "..."}</span>
+      </p>
+    </div>
+  );
+}
+
+function Search({ value, onChange, children }) {
+  return (
+    <div className="container p-0 mb-4">
+      <h1 className="mb-5">React Testing</h1>
+
+      <label htmlFor="search" className="form-label">
+        {children}
+      </label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+        className="form-control"
+      />
     </div>
   );
 }
